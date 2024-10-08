cbfdf635a9da78d3e9f371d619666f6d9ce586aa
VictoriaR <vitoriaalves.ajx@gmail.com>
2024-10-08 13:35:00 -0300

    Commit: Tela de cadastro e login

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,35 @@ namespace ProjetoAgenda
         {
             InitializeComponent();
         }
+
+        private void habilitarBotaoLogin()
+        {
+            //Se o usuário for diferente de vazio e a quantidade de caracteres na Senha for maior que 8 habilita o botão de login
+            if (txtVisorUsuario.Text.Length > 0 && txtVisorSenha.Text.Length >= 8)
+            {
+                bttnEntrar.Enabled = true;
+            }
+            else
+            {
+                bttnEntrar.Enabled = false;
+            }
+        }
+
+        private void bttnCadastrar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 form = new Form2();
+            form.ShowDialog();
+        }
+
+        private void txtVisorUsuario_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoLogin();
+        }
+
+        private void txtVisorSenha_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoLogin();
+        }
     }
 }
---
+++ Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoAgenda
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void habilitarBotaoCadastrar()
+        {
+            if (txtVisorNome.Text.Length > 0 && txtVisorUsuario.Text.Length > 0 && txtTelefone.Text.Length > 0  && txtSenha.Text.Length >= 8 && txtSenha.Text == txtRepitirSenha.Text)
+            {
+                bttnCadastrar.Enabled = true;
+            }
+            else
+            {
+                bttnCadastrar.Enabled = false;
+            }
+        }
+
+        private void txtVisorNome_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoCadastrar();
+        }
+
+        private void txtVisorUsuario_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoCadastrar();
+        }
+
+        private void txtTelefone_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoCadastrar();
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoCadastrar();
+        }
+
+        private void txtRepitirSenha_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoCadastrar();
+        }
+
+        private void bttnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+        }
+    }
+}
